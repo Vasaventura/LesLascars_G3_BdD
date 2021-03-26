2400e2dcff056b0907b03586ce9f794c78f811c6
Vasaventura <73579662+Vasaventura@users.noreply.github.com>
2021-03-26 10:10:44 +0100

    Add files via upload
    Test pour la base de données initiale

---
+++ vaccination_suisse.sql
@@ -0,0 +1,8 @@
+--Quick Check for existing databases
+DROP DATABASE IF EXISTS vaccination_suisse;
+
+--Create a database
+CREATE DATABASE vaccination_suisse;
+
+-- Use the database bus_horaire to execute code manually
+USE vaccination_suisse;
